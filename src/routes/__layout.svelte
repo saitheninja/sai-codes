fe054084957a2914c42943cddb97891201156815
sai <saitheninja@gmail.com>
2021-10-28 15:08:05 +0200

    move footer to __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,3 +10,21 @@
 <body class="text-white bg-gradient-to-br from-blue-900 to-pink-900 bg-fixed">
   <slot />
 </body>
+
+<footer class="mb-4">
+  <p id="contact" class="gray text-center">
+    <a href="https://github.com/saitheninja/resume" target="_blank">
+      site repo
+    </a>
+    /
+    <a href="mailto:saitheninja@gmail.com"> email </a>
+    /
+    <a href="https://saitheninja.com" target="_blank"> photos </a>
+    /
+    <a href="https://www.instagram.com/saitheninja/" target="_blank">
+      Instagram
+    </a>
+    /
+    <a href="https://twitter.com/sainotbot" target="_blank"> Twitter </a>
+  </p>
+</footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -102,22 +102,6 @@
       pretty websites.
     </p>
   </div>
-
-  <p id="contact" class="gray text-center">
-    <a href="https://github.com/saitheninja/resume" target="_blank">
-      site repo
-    </a>
-    /
-    <a href="mailto:saitheninja@gmail.com"> email </a>
-    /
-    <a href="https://saitheninja.com" target="_blank"> photos </a>
-    /
-    <a href="https://www.instagram.com/saitheninja/" target="_blank">
-      Instagram
-    </a>
-    /
-    <a href="https://twitter.com/sainotbot" target="_blank"> Twitter </a>
-  </p>
 </div>
 
 <style>
