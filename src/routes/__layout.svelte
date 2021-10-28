2dad47a5d953b65cad9a73cd24816f8a605f61a6
sai <saitheninja@gmail.com>
2021-10-28 15:29:36 +0200

    header and footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,30 +7,22 @@
   <link rel="icon" href="https://fav.farm/⌨️" />
 </svelte:head>
 
-<header>
-  <h1>sai.codes</h1>
-  <a href="resume">resume</a>
-  <a href="blog">blog</a>
+<header class="m-4">
+  <a href="/" class="text-4xl font-mono">sai.codes</a>
+  <span class="float-right space-x-2">
+    <a href="resume">resume</a>
+    <a href="blog">blog</a>
+  </span>
 </header>
 
 <body class="text-white bg-gradient-to-br from-blue-900 to-pink-900 bg-fixed">
   <slot />
 </body>
 
-<footer class="mb-4">
-  <p id="contact" class="gray text-center">
-    <a href="https://github.com/saitheninja/resume" target="_blank">
-      site repo
-    </a>
-    /
-    <a href="mailto:saitheninja@gmail.com"> email </a>
-    /
-    <a href="https://saitheninja.com" target="_blank"> photos </a>
-    /
-    <a href="https://www.instagram.com/saitheninja/" target="_blank">
-      Instagram
-    </a>
-    /
-    <a href="https://twitter.com/sainotbot" target="_blank"> Twitter </a>
-  </p>
+<footer class="my-4 space-x-4 gray text-center">
+  <a href="https://github.com/saitheninja/resume" target="_blank">site repo</a>
+  <a href="mailto:saitheninja@gmail.com">email</a>
+  <a href="https://saitheninja.com" target="_blank">photos</a>
+  <a href="https://www.instagram.com/saitheninja/" target="_blank">Instagram</a>
+  <a href="https://twitter.com/sainotbot" target="_blank">Twitter</a>
 </footer>
