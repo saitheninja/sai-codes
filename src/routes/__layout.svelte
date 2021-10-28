f7525d2b0893ac3674bfb86f15406a206fa06d40
sai <saitheninja@gmail.com>
2021-10-28 15:58:44 +0200

    improve tags and position footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,23 +7,28 @@
   <link rel="icon" href="https://fav.farm/⌨️" />
 </svelte:head>
 
-<header class="m-4 flex">
-  <a href="/" class="text-4xl font-mono">sai.codes</a>
-  <div class="flex-grow" />
-  <span id="links" class="space-x-2">
-    <a href="blog">blog</a>
-    <a href="resume">for hire</a>
-  </span>
-</header>
+<body class="flex flex-col min-h-screen">
+  <header class="m-4 flex">
+    <a href="/" class="text-4xl font-mono">sai.codes</a>
+    <div class="flex-grow" />
+    <span id="links" class="space-x-2">
+      <a href="blog">blog</a>
+      <a href="resume">for hire</a>
+    </span>
+  </header>
 
-<body class="">
-  <slot />
+  <main class="flex-grow">
+    <slot />
+  </main>
+
+  <footer class="my-4 space-x-4 gray text-center">
+    <a href="https://github.com/saitheninja/resume" target="_blank">site repo</a
+    >
+    <a href="mailto:saitheninja@gmail.com">email</a>
+    <a href="https://saitheninja.com" target="_blank">photos</a>
+    <a href="https://www.instagram.com/saitheninja/" target="_blank"
+      >Instagram</a
+    >
+    <a href="https://twitter.com/sainotbot" target="_blank">Twitter</a>
+  </footer>
 </body>
-
-<footer class="my-4 space-x-4 gray text-center">
-  <a href="https://github.com/saitheninja/resume" target="_blank">site repo</a>
-  <a href="mailto:saitheninja@gmail.com">email</a>
-  <a href="https://saitheninja.com" target="_blank">photos</a>
-  <a href="https://www.instagram.com/saitheninja/" target="_blank">Instagram</a>
-  <a href="https://twitter.com/sainotbot" target="_blank">Twitter</a>
-</footer>
