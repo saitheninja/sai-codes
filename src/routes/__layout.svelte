892d1396a352542ba8d784421e8ba7b30cdc0ee3
sai <saitheninja@gmail.com>
2021-11-17 21:18:28 +0200

    rename routes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,16 +10,16 @@
 <body class="min-h-screen max-w-prose mx-4 md:mx-auto">
   <header class="pt-4 pb-12 sticky top-0 text-right">
     <h1><a href="/" class="text-4xl font-mono"> sai.codes </a></h1>
-    <span id="links" class="space-x-2 text-sm">
-      <a href="writing"> writing </a>
-      <a href="resume"> for hire </a>
+    <nav id="links" class="mt-1 space-x-2 text-sm">
+      <a href="resume"> resume </a>
+      <a href="ranting"> ranting </a>
       <a href="https://github.com/saitheninja/resume" target="_blank">
         GitHub
       </a>
       <a href="https://www.instagram.com/saitheninja/" target="_blank">
         Instagram
       </a>
-    </span>
+    </nav>
   </header>
 
   <main class="pb-8">
