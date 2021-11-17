debd753f40592559eb80d4f106e9c49a27d8e970
sai <saitheninja@gmail.com>
2021-11-17 22:03:02 +0200

    add link-hover class

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,11 +1,21 @@
-<header class="pt-4 pb-10 pr-6 lg:sticky top-0 text-right text-gray-300">
-  <h1><a href="/" class="text-4xl font-mono"> sai.codes </a></h1>
+<header class="pt-4 pb-10 pr-6 lg:sticky top-0 text-right">
+  <h1><a href="/" class="text-4xl font-mono link-hover"> sai.codes </a></h1>
 
   <nav class="pt-1 space-x-2 text-sm">
-    <a href="resume"> resume </a>
-    <a href="blog"> blog </a>
-    <a href="https://github.com/saitheninja/resume" target="_blank"> GitHub </a>
-    <a href="https://www.instagram.com/saitheninja/" target="_blank">
+    <a class="link-hover" href="resume"> resume </a>
+    <a class="link-hover" href="blog"> blog </a>
+    <a
+      class="link-hover"
+      href="https://github.com/saitheninja/resume"
+      target="_blank"
+    >
+      GitHub
+    </a>
+    <a
+      class="link-hover"
+      href="https://www.instagram.com/saitheninja/"
+      target="_blank"
+    >
       Instagram
     </a>
   </nav>
