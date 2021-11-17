3b5e0f6f08c2794ebbed43cad3ed5912dde32979
sai <saitheninja@gmail.com>
2021-11-17 22:22:00 +0200

    change all heading text to font-mono

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,7 +1,7 @@
-<header class="pt-4 pb-10 pr-6 lg:sticky top-0 text-right">
-  <h1><a href="/" class="text-4xl font-mono link-hover"> sai.codes </a></h1>
+<header class="pt-4 pb-10 pr-6 lg:sticky top-0 text-right font-mono">
+  <h1><a href="/" class="text-4xl link-hover"> sai.codes </a></h1>
 
-  <nav class="pt-1 space-x-2 text-sm">
+  <nav class="pt-1 space-x-1 text-sm">
     <a class="link-hover" href="resume"> resume </a>
     <a class="link-hover" href="blog"> blog </a>
     <a
