8ef591d8aabb10a27d90cd0b66eaee039b8d3a27
sai <saitheninja@gmail.com>
2021-11-18 04:21:23 +0200

    move header over to the right

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,22 +1,34 @@
-<header class="pt-4 pb-10 pr-6 lg:sticky top-0 text-right font-mono">
-  <h1><a href="/" class="text-4xl link-hover"> sai.codes </a></h1>
+<header class="pt-4 pb-10 px-6 lg:sticky top-0">
+  <h1>
+    <a href="/" class="text-4xl link-hover font-mono font-bold"> sai.codes </a>
+  </h1>
 
-  <nav class="mt-1 space-x-1 text-sm">
-    <a class="link-hover" href="resume"> resume </a>
-    <a class="link-hover" href="blog"> blog </a>
-    <a
-      class="link-hover"
-      href="https://github.com/saitheninja/resume"
-      target="_blank"
-    >
-      GitHub
-    </a>
-    <a
-      class="link-hover"
-      href="https://www.instagram.com/saitheninja/"
-      target="_blank"
-    >
-      Instagram
-    </a>
-  </nav>
+  <aside>
+    <nav class="mt-1 space-x-1 text-sm">
+      <ul>
+        <li><a class="link-hover" href="resume"> resume </a></li>
+
+        <li><a class="link-hover" href="blog"> blog </a></li>
+
+        <li>
+          <a
+            class="link-hover"
+            href="https://github.com/saitheninja/resume"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            class="link-hover"
+            href="https://www.instagram.com/saitheninja/"
+            target="_blank"
+          >
+            Instagram
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </aside>
 </header>
