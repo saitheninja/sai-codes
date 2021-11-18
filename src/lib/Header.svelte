e4f1a90a90a191b917128f372fac7c4fbc530dff
sai <saitheninja@gmail.com>
2021-11-18 04:51:31 +0200

    add link-hover class to all links

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,4 +1,6 @@
-<header class="pt-4 pb-10 px-6 lg:sticky top-0">
+<header
+  class="py-4 mb-10 px-6 lg:sticky top-0 backdrop-filter backdrop-hue-rotate-90 backdrop-blur z-10"
+>
   <h1>
     <a href="/" class="text-4xl link-hover font-mono font-bold"> sai.codes </a>
   </h1>
@@ -6,25 +8,18 @@
   <aside>
     <nav class="mt-1 space-x-1 text-sm">
       <ul>
-        <li><a class="link-hover" href="resume"> resume </a></li>
+        <li><a href="resume"> resume </a></li>
 
-        <li><a class="link-hover" href="blog"> blog </a></li>
+        <li><a href="blog"> blog </a></li>
 
         <li>
-          <a
-            class="link-hover"
-            href="https://github.com/saitheninja/resume"
-            target="_blank"
-          >
+          <a href="https://github.com/saitheninja/resume" target="_blank">
             GitHub
           </a>
         </li>
+
         <li>
-          <a
-            class="link-hover"
-            href="https://www.instagram.com/saitheninja/"
-            target="_blank"
-          >
+          <a href="https://www.instagram.com/saitheninja/" target="_blank">
             Instagram
           </a>
         </li>
