6d2ef6f8de2b4693409d35acc6ff63e81fba7f29
sai <saitheninja@gmail.com>
2021-11-18 04:51:58 +0200

    remove aside tag

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -5,25 +5,23 @@
     <a href="/" class="text-4xl link-hover font-mono font-bold"> sai.codes </a>
   </h1>
 
-  <aside>
-    <nav class="mt-1 space-x-1 text-sm">
-      <ul>
-        <li><a href="resume"> resume </a></li>
+  <nav class="mt-1 space-x-1 text-sm">
+    <ul>
+      <li><a href="resume"> resume </a></li>
 
-        <li><a href="blog"> blog </a></li>
+      <li><a href="blog"> blog </a></li>
 
-        <li>
-          <a href="https://github.com/saitheninja/resume" target="_blank">
-            GitHub
-          </a>
-        </li>
+      <li>
+        <a href="https://github.com/saitheninja/resume" target="_blank">
+          GitHub
+        </a>
+      </li>
 
-        <li>
-          <a href="https://www.instagram.com/saitheninja/" target="_blank">
-            Instagram
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </aside>
+      <li>
+        <a href="https://www.instagram.com/saitheninja/" target="_blank">
+          Instagram
+        </a>
+      </li>
+    </ul>
+  </nav>
 </header>
