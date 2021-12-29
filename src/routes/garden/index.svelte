1c34e39bb9120f626046d1f88d93b51f10d3e5d6
sai <saitheninja@gmail.com>
2021-12-30 00:34:15 +0200

    start the garden

--- src/routes/garden/index.svelte
+++ src/routes/garden/index.svelte
@@ -1,13 +1,32 @@
 <h1>The Garden</h1>
+<br />
 
-<p>This is what you should picture in your head while you read:</p>
+<div id="on-gardening" class="space-y-2">
+  <p>
+    This is a digital garden. Here is some context on Digital Gardening from
+    <a href="https://maggieappleton.com/garden-history">Maggie Appleton</a>.
+  </p>
 
-<iframe
-  width="560"
-  height="315"
-  src="https://www.youtube.com/embed/EbPOImtuw2Q"
-  title="Ranting Swede"
-  frameborder="0"
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-  allowfullscreen
-/>
+  <p>
+    This is the place where I grow seeds of ideas into trees, flowers, bonsais,
+    succulents, bushes and vines. It is a place in constant need of pruning and
+    weeding.
+  </p>
+</div>
+<br />
+
+<div id="ranting-swede" class="space-y-2">
+  <p class="text-sm">
+    This is what you should picture in your head while you read a post:
+  </p>
+
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/EbPOImtuw2Q"
+    title="Ranting Swede"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  />
+</div>
