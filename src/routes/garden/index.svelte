0d37678e44491371703c12aaab45245a4371c2bb
sai <saitheninja@gmail.com>
2022-01-06 19:57:50 +0200

    update garden description

--- src/routes/garden/index.svelte
+++ src/routes/garden/index.svelte
@@ -1,20 +1,22 @@
 <h1 class="pb-8">The Garden</h1>
 
-<div id="on-gardening" class="space-y-2 pb-8">
+<div id="on-gardening" class="space-y-4 pb-12">
   <p>
     This is a digital garden. Here is some context on Digital Gardening from
-    <a href="https://maggieappleton.com/garden-history">Maggie Appleton</a>.
+    <a href="https://maggieappleton.com/garden-history" target="_blank"
+      >Maggie Appleton</a
+    >.
   </p>
 
   <p>
     This is the place where I grow seeds of ideas into colourful flowers and
-    towering trees. There are some carefully pruned bonsais amongst the wild
-    bushes. Be careful of strangling vines.
+    sturdy, thick-limbed trees. There are some carefully pruned bonsais amongst
+    the untamed bushes. Be careful of strangling vines.
   </p>
 </div>
 
-<div id="ranting-swede" class="space-y-2">
-  <p class="italic text-sm text-center">
+<div id="ranting-swede">
+  <p class="italic text-sm text-center pb-1">
     This is what you should picture in your head while reading a post:
   </p>
 
