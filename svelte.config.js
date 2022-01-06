79ec2145462abb2787d07be95a009a26cad5067f
sai <saitheninja@gmail.com>
2022-01-06 18:37:52 +0200

    update tailwind, other packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,33 @@
-import preprocess from 'svelte-preprocess';
+import preprocess from "svelte-preprocess";
 // import adapter from '@sveltejs/adapter-netlify';
-import adapter from '@sveltejs/adapter-static';
-import path from 'path';
+import adapter from "@sveltejs/adapter-static";
+import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [preprocess({
-		postcss: true
-	})],
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
 
-		vite: {
-			resolve: {
-				alias: {
-					$lib: path.resolve('./src/lib'),
-					$routes: path.resolve('./src/routes'),
-				}
-			}
-		}
-	}
+    vite: {
+      resolve: {
+        alias: {
+          $lib: path.resolve("./src/lib"),
+          $routes: path.resolve("./src/routes"),
+        },
+      },
+    },
+  },
 };
 
 export default config;
