81361cbfdb2e73066db22a6e5d292aeff6a8ace7
sai <saitheninja@gmail.com>
2022-02-17 20:05:15 +0200

    header: add contact page and change links; refactor

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,25 +1,12 @@
 <header
-  class="px-6 py-4 mb-10 sticky top-0 backdrop-filter backdrop-hue-rotate-90 backdrop-blur-md z-10 text-right"
+  class="p-4 mb-10 sticky top-0 backdrop-filter backdrop-hue-rotate-90 backdrop-blur-md z-10 text-gray-300"
 >
-  <a href="/" class="text-4xl link-hover font-mono font-bold"> sai.codes </a>
-
-  <nav class="mt-1 space-x-1 text-sm text-gray-300">
-    <ul class="px-2">
+  <nav>
+    <ul>
+      <li><a href="/" class="text-4xl font-mono font-bold"> sai.codes </a></li>
       <li><a href="resume"> resume </a></li>
-
       <li><a href="garden"> garden </a></li>
-
-      <li>
-        <a href="https://github.com/saitheninja/resume" target="_blank">
-          GitHub
-        </a>
-      </li>
-
-      <li>
-        <a href="https://www.instagram.com/saitheninja/" target="_blank">
-          Instagram
-        </a>
-      </li>
+      <li><a href="contact"> contact </a></li>
     </ul>
   </nav>
 </header>
