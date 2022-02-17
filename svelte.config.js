06738a5575b3a74e74d75070cb5bf540afbb65bf
sai <saitheninja@gmail.com>
2022-02-17 19:29:33 +0200

    sveltekit config update

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
 import preprocess from "svelte-preprocess";
-// import adapter from '@sveltejs/adapter-netlify';
 import adapter from "@sveltejs/adapter-static";
 import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: [
     preprocess({
       postcss: true,
@@ -15,9 +12,6 @@ const config = {
 
   kit: {
     adapter: adapter(),
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
 
     vite: {
       resolve: {
