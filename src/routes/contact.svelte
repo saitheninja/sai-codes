4476fa5c5c6966a90bcab0c8fd699322256e4a7f
sai <saitheninja@gmail.com>
2022-02-17 20:04:07 +0200

    make contact page

---
+++ src/routes/contact.svelte
@@ -0,0 +1,14 @@
+<div>
+  <ul>
+    <li>
+      <a href="https://github.com/saitheninja/resume" target="_blank">
+        GitHub
+      </a>
+    </li>
+    <li>
+      <a href="https://www.instagram.com/saitheninja/" target="_blank">
+        Instagram
+      </a>
+    </li>
+  </ul>
+</div>
