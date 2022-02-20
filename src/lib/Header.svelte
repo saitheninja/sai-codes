39bbb6eaa93c7d0da5f4a80ac52097527d1839b4
sai <saitheninja@gmail.com>
2022-02-21 01:03:08 +0200

    header styling

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,8 +2,15 @@
   class="p-4 sticky top-0 backdrop-filter backdrop-hue-rotate-90 backdrop-blur-md z-10 text-gray-300"
 >
   <nav>
-    <ul>
-      <li><a href="/" class="text-4xl font-mono font-bold"> sai.codes </a></li>
+    <ul class="leading-snug">
+      <li>
+        <a
+          href="/"
+          class="text-4xl text-white font-mono font-bold underline-offset-4"
+        >
+          sai.codes
+        </a>
+      </li>
       <li><a href="resume"> resume </a></li>
       <li><a href="garden"> garden </a></li>
       <li><a href="contact"> contact </a></li>
