44a847285d0c4f46357e805ade60964bcf805c7c
sai <saitheninja@gmail.com>
2022-02-21 23:05:39 +0200

    writing

--- src/routes/garden/discord-is-better.svelte
+++ src/routes/garden/discord-is-better.svelte
@@ -1,27 +1,36 @@
-<h1 class="mb-10">Discord is Better Than What We Had Before</h1>
+<h1 class="leading-tight mb-10">Discord is Better Than What We Had Before</h1>
 
 <div class="space-y-4">
   <p>
-    I don't want to look like a asshole. Knowing my messages eventually get
-    deleted relieves some of my posting anxiety. Especially when I ask something
-    obvious or dumb: that stuff is just clutter. (Memes go stale incredibly
-    quickly). Discord vs forums is like texting vs email.
+    I don't want to look like
+    <a
+      href="https://www.youtube.com/watch?v=3nhQmUuPbBs&t=222s"
+      target="_blank"
+      rel="external"
+    >
+      a asshole.
+    </a>
+    Knowing my messages eventually get deleted relieves some of my posting anxiety.
+    Especially when I ask something obvious or dumb - that stuff is just clutter.
+    (Also memes go stale incredibly quickly). Discord vs forums is like texting vs
+    email.
   </p>
 
   <p>
-    Sure, It doesn't take up much space to store messages, but the internet is
-    rotting and those forums aren't immune (especially the images, which usually
-    had to be hosted separately and are therefore long gone).
+    Also, the internet is rotting and those forums aren't immune. Especially the
+    images, which usually had to be hosted on a different service and therefore
+    are long gone.
   </p>
 
   <p>
     I've definitely found value in decades old forum threads, however I also
-    find value in all the nice features Discord has. I'll make the trade for
-    emoji reactions.
+    find value in all the shiny features that Discord has. I'll make the trade
+    for emoji reactions.
   </p>
 
   <p>
     I think most humans find synchronous communication to be easier. Instant
     messaging has always had a widespread appeal that posting on forums has not.
+    Having the instant feedback makes communication easier.
   </p>
 </div>
