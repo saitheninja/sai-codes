8c7f3c7066bc9a19f3f92e634a05091dd1d36634
sai <saitheninja@gmail.com>
2022-02-21 16:13:47 +0200

    tweak header styling

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,18 +2,17 @@
   class="p-4 sticky top-0 backdrop-filter backdrop-hue-rotate-90 backdrop-blur-md z-10 text-gray-300 print:hidden"
 >
   <nav>
-    <ul class="leading-snug">
-      <li>
-        <a
-          href="/"
-          class="text-4xl text-white font-mono font-bold underline-offset-4"
-        >
-          sai.codes
-        </a>
-      </li>
-      <li><a href="resume"> resume </a></li>
-      <li><a href="garden"> garden </a></li>
-      <li><a href="contact"> contact </a></li>
-    </ul>
+    <a
+      href="/"
+      class="text-4xl text-white font-mono font-bold underline-offset-4"
+    >
+      sai.codes
+    </a>
+
+    <div class="space-x-2">
+      <a href="resume">resume</a>
+      <a href="garden">garden</a>
+      <a href="contact">contact</a>
+    </div>
   </nav>
 </header>
