8d24a76fdb50bc133a9cfc4b03dc31947344ea28
sai <saitheninja@gmail.com>
2022-02-21 22:18:22 +0200

    refactor garden page

--- src/routes/garden/index.svelte
+++ src/routes/garden/index.svelte
@@ -1,25 +1,26 @@
 <h1 class="pb-8">The Garden</h1>
 
-<div id="on-gardening" class="space-y-4 pb-12">
+<div id="on-gardening" class="space-y-4 pb-4">
   <p>
-    This is a digital garden. Here is some context on Digital Gardening from
-    <a href="https://maggieappleton.com/garden-history" target="_blank"
-      >Maggie Appleton</a
-    >.
+    Welcome to this digital garden. Here is some context from
+    <a href="https://maggieappleton.com/garden-history" target="_blank">
+      Maggie Appleton.
+    </a>
   </p>
 
-  <p>
-    This is the place where I grow seeds of ideas into colourful flowers and
-    sturdy, thick-limbed trees. There are some carefully pruned bonsais amongst
-    the untamed bushes. Be careful of strangling vines.
+  <p class="text-gray-300 italic">
+    This is a place where seeds of ideas grow into dazzling flowers, and sturdy,
+    thick-limbed trees. There are some carefully pruned bonsai amongst the
+    untamed bushes. Be careful of strangling vines.
   </p>
 </div>
 
-<div id="ranting-swede">
-  <p class="italic text-sm text-center pb-1 text-gray-300">
+<h2 class="pt-8 pb-2">Writing</h2>
+
+<div id="ranting-swede" class="pt-2 pb-4">
+  <p class="italic text-sm pb-1 text-gray-300">
     This is what you should picture in your head while reading a post:
   </p>
-
   <iframe
     src="https://www.youtube.com/embed/EbPOImtuw2Q"
     title="Ranting Swede"
@@ -30,8 +31,10 @@
   />
 </div>
 
-<h2 class="pt-8 pb-2">Writing</h2>
-
-<a href="/garden/discord-is-better">
-  Discord is Better Than What We Had Before
-</a>
+<ul class="list-disc pl-6">
+  <li>
+    <a href="/garden/discord-is-better">
+      Discord is Better Than What We Had Before
+    </a>
+  </li>
+</ul>
