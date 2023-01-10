ef5b7f1d32a35f2b7cb6b2360bafe348ac2c4bb6
sai <saitheninja@gmail.com>
2023-01-10 17:40:20 +0200

    update contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,13 +2,24 @@
   <ul>
     <li>
       <a
-        href="https://github.com/saitheninja/resume"
+        href="https://github.com/saitheninja/"
         target="_blank"
         rel="noreferrer external"
       >
         GitHub
       </a>
     </li>
+
+    <li>
+      <a
+        href="https://saitheninja.itch.io/"
+        target="_blank"
+        rel="noreferrer external"
+      >
+        itch.io
+      </a>
+    </li>
+
     <li>
       <a
         href="https://www.instagram.com/saitheninja/"
@@ -16,6 +27,16 @@
         rel="noreferrer external"
       >
         Instagram
+      </a>
+    </li>
+
+    <li>
+      <a
+        href="https://saitheninja.com"
+        target="_blank"
+        rel="noreferrer external"
+      >
+        photography portfolio
       </a>
     </li>
   </ul>
