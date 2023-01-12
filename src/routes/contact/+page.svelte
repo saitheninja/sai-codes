75fe144b4d6e432ca17aa9710b01720fc7f3b131
sai <saitheninja@gmail.com>
2023-01-12 19:16:37 +0200

    update contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,46 +2,57 @@
   <title>sai.codes, and also other things</title>
 </svelte:head>
 
-<div>
-  <ul>
-    <li>
-      <a
-        href="https://github.com/saitheninja/"
-        target="_blank"
-        rel="noreferrer external"
-      >
-        GitHub
-      </a>
-    </li>
+<h1 class="pb-8">Contact</h1>
 
-    <li>
-      <a
-        href="https://saitheninja.itch.io/"
-        target="_blank"
-        rel="noreferrer external"
-      >
-        itch.io
-      </a>
-    </li>
+<div class="space-y-6">
+  <section>
+    <h2 class="mb-2">Code</h2>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/saitheninja/"
+          target="_blank"
+          rel="noreferrer external"
+        >
+          GitHub
+        </a>
+      </li>
+    </ul>
+  </section>
 
-    <li>
-      <a
-        href="https://www.instagram.com/saitheninja/"
-        target="_blank"
-        rel="noreferrer external"
-      >
-        Instagram
-      </a>
-    </li>
+  <section>
+    <h2 class="mb-2">Art</h2>
 
-    <li>
-      <a
-        href="https://saitheninja.com"
-        target="_blank"
-        rel="noreferrer external"
-      >
-        photography portfolio
-      </a>
-    </li>
-  </ul>
+    <ul>
+      <li>
+        <a
+          href="https://saitheninja.itch.io/"
+          target="_blank"
+          rel="noreferrer external"
+        >
+          itch.io
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="https://www.instagram.com/saitheninja/"
+          target="_blank"
+          rel="noreferrer external"
+        >
+          Instagram
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="https://saitheninja.com"
+          target="_blank"
+          rel="noreferrer external"
+        >
+          photography
+        </a>
+      </li>
+    </ul>
+  </section>
 </div>
