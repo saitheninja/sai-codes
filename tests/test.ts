b1ec4b3f5992abda45ff22dc750653017fbb2650
sai <saitheninja@gmail.com>
2023-01-12 19:35:41 +0200

    add some basic tests

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,20 @@ import { expect, test } from "@playwright/test";
 
 test("index page has expected h1", async ({ page }) => {
   await page.goto("/");
-  await expect(page.locator('h1')).toHaveText("Welcome to SvelteKit");
+  await expect(page.locator("h1")).toHaveText("Hello!");
+});
+
+test("resume page has expected h1", async ({ page }) => {
+  await page.goto("/resume");
+  await expect(page.locator("h1")).toHaveText("Saien Govender");
+});
+
+test("contact page has expected h1", async ({ page }) => {
+  await page.goto("/contact");
+  await expect(page.locator("h1")).toHaveText("Contact");
+});
+
+test("garden page has expected h1", async ({ page }) => {
+  await page.goto("/garden");
+  await expect(page.locator("h1")).toHaveText("The Garden");
 });
