4074b75490da10cc8a43ca43cc8ab68597afd1f1
sai <saitheninja@gmail.com>
2023-02-20 17:36:03 +0200

    tweak margins when printing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,7 +26,7 @@
 
   <main
     id="main"
-    class="mx-6 mt-10 mb-6 max-w-prose flex-grow scroll-mt-36 pb-8 sm:mx-auto sm:mt-14"
+    class="mx-6 mt-10 mb-12 max-w-prose flex-grow scroll-mt-36 print:my-0 print:mx-auto sm:mx-auto sm:mt-14"
   >
     <slot />
   </main>
