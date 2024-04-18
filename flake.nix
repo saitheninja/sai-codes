9fb5442839940b8d81760370ff57d55386f10e93
sai <saitheninja@gmail.com>
2024-04-18 15:06:39 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Nix flake Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
+
+  in {
+    devShells.${system}.default = let
+      pkgs = import nixpkgs { inherit system; };
+      # inherit same as { system = system; };
+
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs_20
+      ];
+
+      shellHook = ''
+        echo "${pkgs.nodejs_20}"
+	      exec zsh
+      '';
+    };
+  };
+}
