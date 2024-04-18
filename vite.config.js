ae8af59c891c6f2abcc31a1ebb6310feee1bd0a2
sai <saitheninja@gmail.com>
2024-04-18 15:31:20 +0200

    update some minor deps, configs. remove old playwright deps, configs.

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
-  test: {
-    include: ["src/**/*.{test,spec}.{js,ts}"],
-    environment: "jsdom",
-    css: true,
-  },
 };
 
 export default config;
