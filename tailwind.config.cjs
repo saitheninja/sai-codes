91ef412b1bfadfba049420d335a6ed23525a3741
sai <saitheninja@gmail.com>
2024-06-13 19:00:59 +0200

    add fonts for mono, sans & serif

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,41 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
+    fontFamily: {
+      mono: [
+        "Cascadia Code",
+        "ui-monospace",
+        "SFMono-Regular",
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        "Liberation Mono",
+        "Courier New",
+        "monospace",
+      ],
+
+      sans: [
+        "Work Sans",
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+
+      serif: [
+        "Yrsa",
+        "ui-serif",
+        "Georgia",
+        "Cambria",
+        "Times New Roman",
+        "Times",
+        "serif",
+      ],
+    },
+
     extend: {},
   },
 
