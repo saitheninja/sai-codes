080b4b48576978102422e0feafee22e31b66284a
sai <saitheninja@gmail.com>
2024-09-03 21:10:49 +0200

    Updates

---
+++ _app/immutable/nodes/6.COzixjGM.js
@@ -0,0 +1,12 @@
+import{s as p,n as i}from"../chunks/scheduler.Ck6cZafl.js";import{S as g,i as v,e as m,s as y,a as c,g as d,c as b,b as u,f as o,d as l}from"../chunks/index.C0dgLUhg.js";function w(f){let e,r="Discord is Better Than What We Had Before",a,s,h=`<p>I don&#39;t want to look like
+    <a href="https://www.youtube.com/watch?v=3nhQmUuPbBs&amp;t=222s" target="_blank" rel="noreferrer external" class="-mr-1">a asshole
+    </a>. Knowing my messages eventually get deleted relieves some of my posting
+    anxiety. Especially when I ask something obvious or dumb - that stuff is
+    just clutter. (Also memes go stale incredibly quickly). Discord vs forums is
+    like texting vs email.</p> <p>Also, the internet is rotting and those forums aren&#39;t immune. Especially the
+    images, which usually had to be hosted on a different service and therefore
+    are long gone.</p> <p>I&#39;ve definitely found value in decades old forum threads, however I also
+    find value in all the shiny features that Discord has. I&#39;ll make the trade
+    for emoji reactions.</p> <p>I think most humans find synchronous communication to be easier. Instant
+    messaging has always had a widespread appeal that posting on forums has not.
+    Having the instant feedback makes communication easier.</p>`;return{c(){e=m("h1"),e.textContent=r,a=y(),s=m("div"),s.innerHTML=h,this.h()},l(t){e=c(t,"H1",{class:!0,"data-svelte-h":!0}),d(e)!=="svelte-m9y628"&&(e.textContent=r),a=b(t),s=c(t,"DIV",{class:!0,"data-svelte-h":!0}),d(s)!=="svelte-iuh3yl"&&(s.innerHTML=h),this.h()},h(){u(e,"class","heading-size-1 mb-10 leading-tight"),u(s,"class","space-y-4")},m(t,n){o(t,e,n),o(t,a,n),o(t,s,n)},p:i,i,o:i,d(t){t&&(l(e),l(a),l(s))}}}class _ extends g{constructor(e){super(),v(this,e,null,w,p,{})}}export{_ as component};
---
+++ _app/immutable/nodes/7.BRgE_43C.js
@@ -0,0 +1,86 @@
+import{s as I,n as j}from"../chunks/scheduler.Ck6cZafl.js";import{S as A,i as w,j as S,k as D,m as E,t as H,l as z,n as T,e as m,s as b,a as u,g as v,c as y,b as f,f as p,d as c}from"../chunks/index.C0dgLUhg.js";import{A as V}from"../chunks/ArticleBase.B07BcFWY.js";function B(g){let e,n=`<h2 id="scales" class="heading-size-2">Scales</h2> <section class="space-y-4"><h3 id="diatonic-scales" class="heading-size-3">Diatonic Scales</h3> <div><p>Each note letter is only used once in a scale.</p> <p>(This convention sometimes leads to notes being described with
+          unexpected sharps and flats, e.g. C Minor Scale has a Bb instead of
+          A.)</p></div> <p>a diatonic scale is any heptatonic scale (scale with 7 notes per octave)
+        that includes five whole steps and two half steps in each octave, in
+        which the two half steps are separated from each other by either two or
+        three whole steps. This pattern ensures that all the half steps are
+        maximally separated from each other (i.e. separated by at least two
+        whole steps).</p> <p>The seven pitches of any diatonic scale can also be obtained by using a
+        chain of six perfect fifths.</p> <div><p>if one of the two versions is a perfect interval, the other is called
+          either diminished (i.e. narrowed by one semitone) or augmented (i.e.
+          widened by one semitone).</p> <p>Otherwise, the larger version is called major, the smaller one minor.</p></div> <div><p>Within a diatonic scale, unisons and octaves are always qualified as
+          perfect,</p> <p>fourths as either perfect or augmented,</p> <p>fifths as perfect or diminished,</p> <p>and all the other intervals (seconds, thirds, sixths, sevenths) as
+          major or minor.</p></div> <section class="space-y-4"><h4 id="major-scale" class="heading-size-4">Major Scale</h4> <p>Also known as the Ionian Mode.</p> <pre>(whole step or half step)
+(semitone steps)
+(total semitones)
+(scale degree, b or # relative to major)
+(note letter)
+(scale names, mode name)
+
+ --W--W-H--W--W--W-H
+ --2--2-1--2--2--2-1
+0--2--4-5--7--9--11-12
+1--2--3-4--5--6--7-1
+C--D--E-F--G--A--B-C
+C Major, Ionian Mode</pre> <p>C Major (and its relative minor scale, A Minor) doesn&#39;t have any
+          sharps or flats.</p></section> <section class="space-y-4"><h4 id="minor-scales" class="heading-size-4">Minor Scales</h4> <div><p>There are 3 types of minor scale:</p> <ul class="list-disc pl-6"><li>natural</li> <li>harmonic</li> <li>melodic</li></ul></div> <section class="space-y-2"><h5 id="natural-minor-scale" class="heading-size-5">Natural Minor Scale</h5> <p>The default minor scale. Assume this minor scale if the type of
+            minor scale is not specified.</p> <pre> --W-H --W--W-H --W --W
+ --2-1 --2--2-1 --2 --2
+0--2-3 --5--7-8 --10--12
+1--2-b3--4--5-b6--b7--1
+C--D-Eb--F--G-Ab--Bb--C
+C Natural Minor, C Minor, Aeolian Mode</pre></section> <section class="space-y-2"><h5 id="harmonic-minor-scale" class="heading-size-5">Harmonic Minor Scale</h5> <p>The seventh scale degree is raised by a half step (back to how it
+            was in the Major Scale, because that interval sounds good).</p> <pre> --W-H --W--W-H ---WH-H
+ --2-1 --2--2-1 ---3 -1
+0--2-3 --5--7-8 ---11-12
+1--2-b3--4--5-b6---7 -1
+C--D-Eb--F--G-Ab---B -C
+C Harmonic Minor</pre></section> <section class="space-y-2"><h5 id="melodic-minor-scale" class="heading-size-5">Melodic Minor Scale</h5> <p>Descending: identical to natural minor.</p> <p>Ascending: raised sixth and seventh scale degrees (so that there
+            isn&#39;t a 3 semitone jump).</p> <pre> --W-H --W--W--W--W-H
+ --2-1 --2--2--2--2-1
+0--2-3 --5--7--9--11-12
+1--2-b3--4--5--6--7-1
+C--D-Eb--F--G--A--B-C
+C Melodic Minor</pre></section> <div><p>They share the flat third scale degree (b3).</p> <p>Major -&gt; Natural Minor</p> <p>1 2 b3 4 5 b6 b7 1</p> <p>Major -&gt; Harmonic Minor</p> <p>1 2 b3 4 5 b6 7 1</p> <p>Major -&gt; Melodic Minor</p> <p>1 2 b3 4 5 6 7 1</p></div> <div><p>A Major to A Minor is the parallel minor.</p> <p>C Major to A Minor is the relative minor.</p> <p>C Major</p> <p>C D E F G A B C</p> <p>C Minor</p> <p>C D Eb F G Ab Bb C</p> <p>A Major</p> <p>A B C# D E F# G# A</p> <p>A Minor (same notes as C Major)</p> <p>A B C D E F G A</p></div> <div><p>Relative minor starts from the 6th note of the major scale.</p> <p>Or, relative major starts from the 3rd note of the minor scale.</p> <p>In other words, the relative minor is 3 semitones below, or 9
+            semitones above, the major.</p> <p>relative minor is a natural minor scale.</p> <p>Natural minor scales share a key signature with a relative major
+            key.</p> <p>The primary difference between major scales and minor scales is the
+            third scale degree. A major scale always has a natural third (or
+            major third). A minor scale never has a major third.</p> <p>In practice, you can add any note as a tension to a minor scale
+            except for a major third.</p> <p>For example, if you are playing a D minor scale, almost any note can
+            arguably sound good in this scale except for F♯. If you play an F♯,
+            the scale instantly takes on a major tonality.</p></div></section></section> <section class="space-y-4"><h3 id="other-scales" class="heading-size-3">Other Scales</h3> <section class="space-y-2"><h4 id="chromatic-scale" class="heading-size-4">Chromatic Scale</h4> <p>All possible notes.</p> <p>12 notes, then repeat.</p> <pre>-H-H-H-...-H- (whole step or half step)
+1-2-3-...-12-1 (note number)
+0-1-2-...-12-13 (total semitones)
+C, C#, D, D#, E, F, F#, G, G#, A, A#, B, C
+(note name, from C for piano)
+E, F, F#, G, G#, A, A#, B, C, C#, D, D#, E
+(note name, from E for guitar)</pre></section> <section class="space-y-2"><h4 id="pentatonic-scale" class="heading-size-4">Pentatonic Scale</h4> <p>5 notes. Remove the notes that are only a semitone away, so that none
+          of the notes clash and you can play them in any order.</p> <div><p>Major -&gt; Pentatonic</p> <p>1 2 3 5 6 (1)</p></div> <div><p>Major -&gt; Minor Pentatonic</p> <p>1 b3 4 5 b7 (1)</p></div></section> <section class="space-y-2"><h4 id="blues-scale" class="heading-size-4">Blues Scale</h4> <p>Minor Pentatonic + b5.</p> <div><p>Major -&gt; Blues</p> <p>1 b3 4 b5 6 b7 (1)</p></div></section></section> <section class="space-y-4"><h3 id="scale-degrees" class="heading-size-3">Scale Degrees</h3> <p>Scale degree: the position of a note relative to the tonic.</p> <pre>I  ii  iii  IV  V  vi  vii° I</pre> <div><p>Uppercase: major</p> <p>Lowercase: minor</p> <p>Lowercase°: diminished</p> <p>Uppercase+: augmented</p></div> <div><p>I: major</p> <p>ii: minor</p> <p>iii: minor</p> <p>IV: major</p> <p>V: major</p> <p>vi: minor</p> <p>vii° (a.k.a. vii_b5): diminished (a.k.a. minor b5)</p> <p>I: major</p></div> <div><p>The eight degrees of the scale are also known by traditional names:</p> <p>1st: Tonic (key note)</p> <p>2nd: Supertonic</p> <p>3rd: Mediant</p> <p>4th: Subdominant</p> <p>5th: Dominant</p> <p>6th: Submediant</p> <p>7th: Leading Note/Tone OR Subtonic</p> <p>8th: Tonic (octave)</p></div> <p>&quot;subtonic&quot; is used when the interval between it and the tonic in the
+        upper octave is a whole step (i.e. natural minor scale).</p> <p>&quot;leading note&quot; is used when that interval is a half-step (i.e. major
+        scale).</p> <pre>                      Tonic
+      (-2nd) Subtonic --|-- (+2nd) Supertonic
+   (-3rd) Submediant ---|--- (+3rd) Mediant
+(-5th) Subdominant -----|----- (+5th) Dominant</pre></section>`,a,i,l=`<h2 id="modes" class="heading-size-2">Modes</h2> <pre>I--II--III-IV--V--VI--VII-I
+ --W --W  -H --W--W --W  -H</pre> <div><p>(starting note): (name of mode)</p> <p>I: Ionian (Major scale)</p> <p>ii: Dorian</p> <p>iii: Phrygian</p> <p>IV: Lydian</p> <p>V: Mixolydian</p> <p>vi: Aeolian (Natural Minor scale)</p> <p>vii: Locrian</p></div> <p>mode: which note to start from in the loop.</p>`,h,t,C=`<h2 id="chords" class="heading-size-2">Chords</h2> <section class="space-y-4"><h3 id="building-chords" class="heading-size-3">Building Chords</h3> <section class="space-y-2"><h4 id="major-chord" class="heading-size-4">Major Chord</h4> <pre>1, 3, 5 (scale degree of major scale)
+-4-3- (steps in semitones)
+0, 4, 7 (total semitones)</pre></section> <section class="space-y-2"><h4 id="minor-chord" class="heading-size-4">Minor Chord</h4> <pre>1, b3, 5
+-3-4-
+0, 3, 7</pre></section> <section class="space-y-2"><h4 id="augmented-chord" class="heading-size-4">Augmented Chord</h4> <pre>1, 3, 5#
+-4-4-
+0, 4, 8</pre></section> <section class="space-y-2"><h4 id="diminished-chord" class="heading-size-4">Diminished Chord</h4> <pre>1, b3, b5
+-3-3-
+0, 3, 6</pre></section> <section class="space-y-2"><h4 id="seventh-chord" class="heading-size-4">(Dominant) Seventh Chord</h4> <pre>1, 3, 5, b7
+-4-3-3-
+0, 4, 7, 10</pre></section> <section class="space-y-2"><h4 id="suspended-2nd-chord" class="heading-size-4">Suspended 2nd Chord</h4> <pre>1, 2, 5
+-2-5-
+0, 2, 7</pre></section> <section class="space-y-2"><h4 id="sus-4-chord" class="heading-size-4">Sus 4 Chord</h4> <pre>1, 4, 5
+-5-2-
+0, 5, 7</pre></section> <section class="space-y-2"><h4 id="major-7-sus-2-chord" class="heading-size-4">Major 7 Sus 2 Chord</h4> <pre>1, 2, 5, 7
+-2-5-2-
+0, 2, 7, 11</pre> <p>(Dmaj7sus2 sounds good)</p></section></section> <section class="space-y-4"><h3 id="chord-progressions" class="heading-size-3">Chord Progressions</h3> <p>These are some common chord progressions.</p> <pre>I-V-vi-IV (0-7-9m-5)</pre> <pre>I-IV-V-IV (0-5-7-5)</pre> <pre>ii-V-I (2m-7-0)</pre> <pre>I-vi-IV-V (0-9m-5-7)</pre> <pre>I-bVII-I (0-10-0)</pre> <div><p>12 Bar Blues</p> <pre>I-I-I-I (0-0-0-0)
+IV-IV-I-I (5-5-0-0)
+V-IV-I-I (7-5-0-0)</pre></div> <div><p>Canon</p> <pre>I-V-vi-iii (0-7-9m-4m)
+IV-I-IV-V (5-0-5-7)</pre></div></section>`,d,o,M=`<h2 id="guitar-notes-math" class="heading-size-2">Guitar Notes Math</h2> <div><p>E-A-D-G-B-E</p> <p>-5-5-5-4-5-</p> <p>0, 5, 10, 15, 19, 24 semitones total (i.e. 2 octaves)</p></div> <div><p>E, F, F#, G, G#, A - 5 semitones</p> <p>A, A#, B, C, C#, D - 5 semitones</p> <p>D, D#, E, F, F#, G - 5 semitones</p> <p>G, G#, A, A#, B - 4 semitones</p> <p>B, C, C#, D, D#, E - 5 semitones</p></div> <p>E, F, F#, G, G#, A, A#, B, C, C#, D, D#, E</p> <div><p>E Major Scale</p> <p>E, F#, G#, A, B, C#, D#, E</p></div> <section class="space-y-4"><h3 id="make-the-c-major-scale" class="heading-size-3">Make the C Major Scale</h3> <p>Barre on fret 1 for 1st and 2nd string.</p> <pre>E A D G B E
+       |--&gt; one semitone up for 1st and 2nd string
+E A D G C F</pre> <pre>C  D E F  G A B (C Major scale)
+2# 4 6 1# 3 5 2 (string number)</pre></section>`;return{c(){e=m("section"),e.innerHTML=n,a=b(),i=m("section"),i.innerHTML=l,h=b(),t=m("section"),t.innerHTML=C,d=b(),o=m("section"),o.innerHTML=M,this.h()},l(s){e=u(s,"SECTION",{class:!0,"data-svelte-h":!0}),v(e)!=="svelte-endipp"&&(e.innerHTML=n),a=y(s),i=u(s,"SECTION",{class:!0,"data-svelte-h":!0}),v(i)!=="svelte-l43nnq"&&(i.innerHTML=l),h=y(s),t=u(s,"SECTION",{class:!0,"data-svelte-h":!0}),v(t)!=="svelte-1wldzjk"&&(t.innerHTML=C),d=y(s),o=u(s,"SECTION",{class:!0,"data-svelte-h":!0}),v(o)!=="svelte-1unwyjd"&&(o.innerHTML=M),this.h()},h(){f(e,"class","space-y-4"),f(i,"class","space-y-4"),f(t,"class","space-y-4"),f(o,"class","space-y-4")},m(s,r){p(s,e,r),p(s,a,r),p(s,i,r),p(s,h,r),p(s,t,r),p(s,d,r),p(s,o,r)},p:j,d(s){s&&(c(e),c(a),c(i),c(h),c(t),c(d),c(o))}}}function G(g){let e,n;return e=new V({props:{titleHeading:"Music Theory Notes",$$slots:{default:[B]},$$scope:{ctx:g}}}),{c(){S(e.$$.fragment)},l(a){D(e.$$.fragment,a)},m(a,i){E(e,a,i),n=!0},p(a,[i]){const l={};i&1&&(l.$$scope={dirty:i,ctx:a}),e.$set(l)},i(a){n||(H(e.$$.fragment,a),n=!0)},o(a){z(e.$$.fragment,a),n=!1},d(a){T(e,a)}}}class x extends A{constructor(e){super(),w(this,e,null,G,I,{})}}export{x as component};
