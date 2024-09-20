342fdcdc531923edd1b4973940210b6d7da1bb01
sai <saitheninja@gmail.com>
2024-09-20 17:52:32 +0200

    garden: update On CSS
    Use ArticleBase.
    Update some article text.

--- src/routes/garden/on-css/+page.svelte
+++ src/routes/garden/on-css/+page.svelte
@@ -1,51 +1,71 @@
-<h1 class="heading-size-1 mb-10 leading-tight">On CSS</h1>
+<script lang="ts">
+  import ArticleBase from "$lib/ArticleBase.svelte";
+</script>
 
-<div class="space-y-4">
-  <p>
-    "pixel perfect" is dumb. Web pages are stretchy. Use min and max, and let
-    the browser work it out.
-  </p>
+<ArticleBase titleHeading="On CSS" titleSubheading="I have *opinions*.">
+  <section class="space-y-4">
+    <h2
+      id="in-general"
+      class="heading-size-2"
+    >
+      In General
+    </h2>
 
-  <p>Margins collapse. Padding adds.</p>
+    <p>
+      "pixel perfect" is dumb. Web pages are stretchy. Set the min and max, and
+      let the browser work it out.
+    </p>
 
-  <p>
-    Container queries are swag af. They allow you to tune layouts more finely
-    than before.
-  </p>
+    <p>Margins collapse. Padding adds.</p>
 
-  <p>
-    Get feedback as fast as possible, so you can fix it before things are built
-    on top of it. I like to build things directly in the browser so I can
-    immediately see where ideas don't work. (Especially fancy layering things,
-    like the <a
-      href="https://github.com/saitheninja/sai-codes/blob/main/src/lib/Header.svelte"
-      >header</a
-    > of this site.)
-  </p>
+    <p>Design layouts from smallest screen to biggest screen.</p>
 
-  <h2 class="heading-size-2 pt-4">Tailwind Specific</h2>
+    <p>
+      Get feedback as fast as possible, so you can fix bugs before things are
+      built on top of it. I like to build things directly in the browser so I
+      can immediately see where ideas don't work. (Especially fancy layering
+      things, like the <a
+        href="https://github.com/saitheninja/sai-codes/blob/main/src/lib/Header.svelte"
+        >header</a
+      > of this site.)
+    </p>
+  </section>
 
-  <p>
-    It's fast to play with designs, and you know you aren't fucking up the rest
-    of the site by changing global CSS. Just doing combinations of static
-    utility classes.
-  </p>
+  <section class="space-y-4">
+    <h2
+      id="tailwind"
+      class="heading-size-2"
+    >
+      Tailwind
+    </h2>
 
-  <p>Tailwind sets useful media queries.</p>
+    <p>
+      It's fast to play with designs, and you know you aren't messing up the
+      rest of the site by changing global CSS, because it is just combinations
+      of isolated utility classes.
+    </p>
 
-  <p>Design all layouts from smallest screen to biggest screen.</p>
+    <p>
+      Tailwind has <a
+        href="https://tailwindcss.com/docs/hover-focus-and-other-states#media-and-feature-queries"
+        target="_blank">useful media queries</a
+      >.
+    </p>
 
-  <p>
-    Build everything in one page and then abstract into components once the
-    patterns become obvious.
-  </p>
+    <p>
+      Build everything in one page and then abstract into components once the
+      patterns become obvious.
+    </p>
 
-  <p>
-    Use margin for fine-tuning specific content, like text or logos. First use
-    <code>gap</code>, <code>space-x</code>, etc.
-  </p>
+    <p>
+      Use margin for fine-tuning specific content, like text or logos. First use
+      <code>gap</code>, <code>space-x</code>, etc.
+    </p>
 
-  <p><code>@apply</code> to use Tailwind classes in .css files.</p>
+    <p>
+      <code>@apply</code> to use Tailwind classes in <code>.css</code> files.
+    </p>
 
-  <p>New coloured shadows are cool.</p>
-</div>
+    <p>Coloured shadows: they are pretty cool.</p>
+  </section>
+</ArticleBase>
