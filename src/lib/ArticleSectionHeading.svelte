6f2a6cd1c75e816c184f60493e73765122cf1e0c
sai <saitheninja@gmail.com>
2024-10-14 14:56:09 +0200

    ArticleSectionHeading: use svelte:element
    `svelte:element` lets you render an element of a dynamically specified type.
    Use to reduce duplication.

--- src/lib/ArticleSectionHeading.svelte
+++ src/lib/ArticleSectionHeading.svelte
@@ -20,40 +20,7 @@
 {:else}
   {@const id = titleToID(title)}
 
-  {#if level === 2}
-    <h2
-      {id}
-      class="heading-size-2"
-    >
+  <svelte:element {id} this={"h" + level} class={"heading-size-" + level}>
       <a href="#{id}">{title}</a>
-    </h2>
-  {:else if level === 3}
-    <h3
-      {id}
-      class="heading-size-3"
-    >
-      <a href="#{id}">{title}</a>
-    </h3>
-  {:else if level === 4}
-    <h4
-      {id}
-      class="heading-size-4"
-    >
-      <a href="#{id}">{title}</a>
-    </h4>
-  {:else if level === 5}
-    <h5
-      {id}
-      class="heading-size-5"
-    >
-      <a href="#{id}">{title}</a>
-    </h5>
-  {:else if level === 6}
-    <h6
-      {id}
-      class="heading-size-6"
-    >
-      <a href="#{id}">{title}</a>
-    </h6>
-  {/if}
+  </svelte:element>
 {/if}
